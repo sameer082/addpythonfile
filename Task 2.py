855cce9bc413e08bfdbbc9cfeedfade5334be5bd
Sameer Maharana <sameermaharana082@gmail.com>
2025-11-20 19:22:08 +0530

    Add files via upload

---
+++ Task 2.py
@@ -0,0 +1,8 @@
+"""Well coming the new users to python coding class
+,by question their full name"""
+
+First_name =input("Enter your first name:")
+Last_name=input("Enter your last name:")
+add=First_name+" "+Last_name
+
+print("Hello",",",add,"Wellcome to python program class here we are learning python.")
