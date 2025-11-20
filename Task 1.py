75579ecdffbe506534d18975ae4319918a0b6a60
Sameer Maharana <sameermaharana082@gmail.com>
2025-11-20 18:48:17 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,16 @@
+"""
+Taking two numbers from users to do mathematical operation like
+Addition, Subtraction, Multiplication and Division
+"""
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number:"))
+
+Addition=num1 + num2
+Subtraction=num1-num2
+Multiplication=num1*num2
+Division=num1//num2
+
+print("Addition:",Addition)
+print("Subtraction:",Subtraction)
+print("Multiplication:",Multiplication)
+print("Division:",Division)
