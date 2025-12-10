e3fbb2147747c8ba60fd0b534229eba0916e2ae6
Sameer Maharana <sameermaharana082@gmail.com>
2025-12-10 23:10:23 +0530

    Update Task 2.py

--- Task 2.py
+++ Task 2.py
@@ -1,1 +1,19 @@
+import math
+
+"Taking the number from the user"
+num = float(input("Enter a number:"))
+
+# As we know
+square_root = math.sqrt(num)
+
+logarithm = math.log(num)
+
+sine_value = math.log(num)
+
+# Results are below
+print(f"Square root: {square_root}")
+print(f"Logarithm : {logarithm}")
+print(f"sine: {sine_value}")
+
+
 
