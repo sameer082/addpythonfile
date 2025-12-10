6e0550990b70c0ccd91ed455d812a043d60286a3
Sameer Maharana <sameermaharana082@gmail.com>
2025-12-10 23:05:14 +0530

    Update Task 2.py

--- Task 2.py
+++ Task 2.py
@@ -1,10 +1,1 @@
-"""
-Adding the numbers
-from 1 to 50 , and showing the result
-for total sum
-"""
 
-total_sum = 0
-for number in range(1, 51):
-    total_sum += number
-print("The sum of numbers from 1 to 51 is :", total_sum)
